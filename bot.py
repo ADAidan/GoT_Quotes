5425199ca1c61272f4a5ffd4d47e34fbc15c924b
Aidan Dyer <100196389+ADAidan@users.noreply.github.com>
2022-03-14 07:08:15 -0400

    Create bot.py
    Main file for the bot

---
+++ bot.py
@@ -0,0 +1,27 @@
+import os
+import discord
+import random
+from dotenv import load_dotenv
+from discord.ext import commands
+from quotes import quotes
+
+intents = discord.Intents.default()
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+
+@bot.command(name='got-quote', help='Gives a random Game of Thrones quote')
+async def random_quote(message):
+    quote, author = random.choice(list(quotes.items()))
+    full_quote = quote + ' ' + author
+    await message.send(full_quote)
+
+
+bot.run(TOKEN)
